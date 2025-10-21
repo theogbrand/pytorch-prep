906916e73fc6861621b2d03771335f48cbe51eaf
ob1 <brandonong@aisingapore.org>
2025-10-21 18:16:52 +0800

    SA Head

--- nn_zero_to_hero/gpt2_pytorch/from_scratch.py
+++ nn_zero_to_hero/gpt2_pytorch/from_scratch.py
@@ -4,6 +4,7 @@ from torch.nn import functional as F
 
 n_embd = 384
 dropout_p = 0.2
+block_size = 256 # context length
 
 class FFN(nn.module):
     def __init__(self):
@@ -14,9 +15,30 @@ class FFN(nn.module):
             nn.Linear(4 * n_embd, n_embd),
             nn.Dropout(dropout_p),
         )
-    def _forward(self, x):
+    def forward(self, x):
         return self.net(x)
 
+class SelfAttentionHead(nn.module):
+    def __init__(self, head_size):
+        super().__init__()
+        self.queries = nn.Linear(n_embd, head_size, bias=False)
+        self.keys = nn.Linear(n_embd, head_size, bias=False)
+        self.values = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, x):
+        B,T,C = x.shape
+        Q = self.queries(x)
+        K = self.keys(x)
+        qk_t = Q @ K.transpose(-2,-1) / C**-0.5 # don't forget negative
+        wei = qk_t.masked_fill(self.tril[:T, :T] == 0, "-inf") # decoder block, cannot tend to future tokens
+        wei = F.softmax(wei, dim=-1) # T,T
+        wei = self.dropout(wei)
+        
+        V = self.values(x)
+        out = wei @ V
+        return out
 
 class GPTLanguageModel(nn.Module):
     def __init__(self):
